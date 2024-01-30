c782f1111e2c9ae3e30ba48d00e39632c3219c3b
Daniel Cohen <danieljcoh@gmail.com>
2024-01-29 20:25:50 -0800

    moved files around

---
+++ Day 26/main.py
@@ -0,0 +1,38 @@
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+#
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
+#
+# #TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+#
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+import pandas
+
+wok_people = {
+    "Kaladin": 100,
+    "Shallan": 0,
+    "Dalinar": 42,
+    "Navani": 72
+}
+
+wok_dataf = pandas.DataFrame(wok_people)
+
+
